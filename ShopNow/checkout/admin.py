8dfbd52e01c14993062d716f57ba7cff0a8da112
RaoMubashir5 <mubashir.qadeer@arbisoft.com>
2024-09-09 16:17:41 +0500

    added id in admin model site

--- ShopNow/checkout/admin.py
+++ ShopNow/checkout/admin.py
@@ -3,7 +3,7 @@ from .models import checkoutPage
 
 # Custom admin configuration for checkoutPage
 class CheckoutPageAdmin(admin.ModelAdmin):
-    list_display = ('full_name', 'address', 'city', 'province', 'zip_code', 'country')  # Specify the fields you want to display
+    list_display = ('id','full_name', 'address', 'city', 'province', 'zip_code', 'country')  # Specify the fields you want to display
     search_fields = ('full_name', 'city', 'province', 'country')  # Add search functionality
     list_filter = ('province', 'country')  # Filter options in the admin list
 
