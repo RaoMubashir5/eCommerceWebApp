7716941c5edf788e23ab4e3f9b1f7b26aeebaa19
RaoMubashir5 <mubashir.qadeer@arbisoft.com>
2024-09-09 14:40:41 +0500

    safe side

--- ShopNow/order/views.py
+++ ShopNow/order/views.py
@@ -21,10 +21,10 @@ from rest_framework.permissions import IsAuthenticated
 from .customPermissions import CustomizeAPIPermissions
 
 class orderApi(APIView):
-    authentication_classes=['JWTAuthentication']
-    permission_classes=['CustomizeAPIPermissions']
+    authentication_classes=[JWTAuthentication]
+    permission_classes=[CustomizeAPIPermissions]
     def get(self,request):
-        if request.data:
+        # if request.data:
             requesting_user=request.user
             order_history=requesting_user.user_orders.all()
             checkout_info_obj=order_history.checkout_info
@@ -33,24 +33,27 @@ class orderApi(APIView):
             response_to_send={'serialized_checkout_info':serialized_checkout_info,
                             'serialized_order':serialized_order, }
             return Response(response_to_send,status=status.HTTP_200_OK)
-        return Response("your request is empty.",status=status.HTTP_400_BAD_REQUEST)
+       # return Response("your request is empty.",status=status.HTTP_400_BAD_REQUEST)
     
     def post(self,request):
-        if request.data:
+        #if request.data:
             requesting_user=request.user
             request_dict=request.data
-            checkout_id=request.get('checkout_page_id') #pass param
-            bill=request.get('total_bill')#pass in query param
-            checkout_obj=checkoutPage.objects.get(id=int(checkout_id))
+            print("yahantak ok ha")
+            checkout_id=request.POST.get('checkout_page_id') #pass param
+            bill=request.POST.get('total_bill')#pass in query param
+            print("02 yahan tak ok ha")
+            checkout_obj=checkoutPage.objects.get(id=checkout_id)
             #adding the param data into the request.data
             request_dict['user_ordering']=requesting_user
             request_dict['checkout_info']=checkout_obj
             request_dict['total_bill']=bill
-
+            print("03 yahantak ok ha")
             serialized=orderSerializer(data=request_dict)
             if serialized.is_valid():
+                serialized.save()
                 return Response("Your Order has been placed!!",status=status.HTTP_201_CREATED)
-        return Response("your request is empty.",status=status.HTTP_400_BAD_REQUEST)
+        #return Response("your request is empty.",status=status.HTTP_400_BAD_REQUEST)
 
         
 
