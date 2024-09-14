df51663b3bcb3920f8835f1bce1e24af79971172
RaoMubashir5 <mubashir.qadeer@arbisoft.com>
2024-09-14 13:16:27 +0500

    Renamed the your_CART and added order items in order model

--- ShopNow/order/models.py
+++ ShopNow/order/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from UserApp.models import Webuser
 
 from checkout.models import checkoutPage
+from product.models import product
 # Create your models here.
 
 class order(models.Model):
@@ -13,4 +14,12 @@ class order(models.Model):
 
 
 
+class OrderItem(models.Model):
+    order = models.ForeignKey(order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(product, on_delete=models.SET_NULL, null=True)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # Store product price at the time of purchase
+
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name}"
 
