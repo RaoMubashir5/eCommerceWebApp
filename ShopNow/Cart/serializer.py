8d27dbb8942244bee37be838e2d64826a6c978e1
RaoMubashir5 <mubashir.qadeer@arbisoft.com>
2024-09-18 11:00:45 +0500

    updated the cart api's as if user clicks on same product addtoCart button multiple times it adds that product exacly same number of times in the cart.

--- ShopNow/Cart/serializer.py
+++ ShopNow/Cart/serializer.py
@@ -14,6 +14,24 @@ class AddToCartSerializer(serializers.ModelSerializer):
     class Meta:
         model=addToCart
         fields='__all__'
-    
+        
+    # def create(self, validated_data):
+    #     product_in_cart = validated_data.get('cart_product')
+    #     cart = validated_data.get('cart')
 
-    
+    #     #Check if the product is already in the cart
+    #     existing_cart_item = addToCart.objects.filter(cart=cart, cart_product=product_in_cart).first()
+
+    #     if existing_cart_item:
+    #     #Increment the quantity of the existing product in the cart
+    #         existing_cart_item.product_quantity += 1
+    #         existing_cart_item.save()
+    #         return existing_cart_item
+    #     else:
+    #         #Create a new cart item if the product is not already in the cart
+    #         cart_data = addToCart.objects.create(
+    #             cart_product=product_in_cart,
+    #             cart=cart,
+    #             product_quantity=validated_data.get('product_quantity')
+    #         )
+    #         return cart_data
--- ShopNow/Cart/urls.py
+++ ShopNow/Cart/urls.py
@@ -6,6 +6,6 @@ from .views import *
 
 urlpatterns = [
     # Path to the API URLs
-    path('addToCart/',AddcartView.as_view(),name='cart'), 
-    # path('addToCart/',AddcartView.as_view(),name='cart'),    
+   path('addToCart/',AddcartView.as_view(),name='cart'), 
+   path('addToCart/<int:product_id>',AddcartView.as_view(),name='delete'),   
 ]
